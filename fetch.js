4beaa386229b81052ea5e7c859f77a33310df79c
mikeynoodledoo <152453274+mikeynoodledoo@users.noreply.github.com>
2024-01-30 00:05:09 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -4,13 +4,11 @@ function getQuestions(){
 	fetch('/easy.txt').then(x => x.text()),
 	fetch('/hard.txt').then(x => x.text()),
 	fetch('/expert.txt').then(x => x.text())
-	]).then(allResp => {
-		let easy = allResp[0];
-		let hard = allResp[1];
-		let expert = allResp[2];
+	]).then(([easy, hard, expert]) => {
+		
 		console.log('Easy file content:', easy);
-    console.log('Hard file content:', hard);
-    console.log('Expert file content:', expert);
+		console.log('Hard file content:', hard);
+		console.log('Expert file content:', expert);
 	})
 	
 }
